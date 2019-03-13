e9122846645266f4647298a8f346506f59a8f32f
liorweitz <liorweitz@gmail.com>
2019-03-13 19:24:58 +0200

    lior doesnt know anything

---
+++ commit push.bat
@@ -0,0 +1,5 @@
+git config --global user.email "liorweitz@gmail.com"
+git config --global user.name "liorweitz"
+git add *
+git commit -m "lior doesnt know anything"
+git push
---
+++ pull files.bat
@@ -0,0 +1,3 @@
+git config --global user.email "liorweitz@gmail.com"
+git config --global user.name "liorweitz"
+git pull
