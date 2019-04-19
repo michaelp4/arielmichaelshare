3de638eee95c7bad39c00e11084e6cd2781638ea
liorweitz <liorweitz@gmail.com>
2019-04-19 15:30:08 +0300

    a commit

--- labScript.m
+++ labScript.m
@@ -1,13 +1,14 @@
+function [ret] = labScript(x,y,yerr)
 %Created BY Ofir Arad, Feb-6-2019.
 %This script fits a non-linear model described by variable "test_model" to
 %the data given by {xdata,ydata} with errors {edata}.
  
-test_model='y~(cos(b1*x+b2)^2)';%model to be fitted, coefficeients are of the form b#
+test_model='y~(b1*x+b2)';%model to be fitted, coefficeients are of the form b#
 beta0=[1 1];%starting values for the coefficients
  
 xdata=x; %x-data points
-ydata=y.*(10^-6); %y-data points
-edata=[0.2 0.2 0.2 0.2 0.2].*0.75.*(10^-6); % errors in y-data points
+ydata=y; %y-data points
+edata=yerr; % errors in y-data points
 dd=1./(edata.*edata);
 %creating the model, errors for this model based on sigma^{-2}
 temp_nlm=fitnlm(xdata,ydata,test_model,beta0,'Weights',dd);
@@ -31,15 +32,9 @@ errorbar(xdata,ydata,edata,'b*');
 hold on
 xdiff=(max(xdata)-min(xdata));%just the difference between the x values
 fplot(@(x) (temp_coeff(1,1)*x+temp_coeff(2,1)),[min(xdata)-xdiff/10,max(xdata)+xdiff/10]);
-title('Number of rings vs mirror location','Interpreter','latex');
-ylabel('x[\mum]');
-xlabel('m[Natural Number]');
-% addTrap(x,y,3,'-');
-% addTrap(x,y,6,'r');
-% addTrap(x,y,18,'y')
-text(min(xdata),max(ydata),sprintf('y=b_1x+b_2\nb1=%d\\pm%d\nb2=%d\\pm%d\n\\chi^2=%d\nd.o.f-reduced \\chi^2=%d',temp_coeff(1,1),temp_coeff(1,2),temp_coeff(2,1),temp_coeff(2,2),temp_nlm.SSE,temp_nlm.MSE),'verticalAlignment','top')
+title('Fit test $\chi^2$','Interpreter','latex');
+ylabel('y');
+xlabel('x');
+text(min(xdata),max(ydata),sprintf('y=b_1x+b_2\nb1=%f\\pm%f\nb2=%f\\pm%f\n\\chi^2=%f\nd.o.f-reduced \\chi^2=%f',temp_coeff(1,1),temp_coeff(1,2),temp_coeff(2,1),temp_coeff(2,2),temp_nlm.SSE,temp_nlm.MSE),'verticalAlignment','top')
 hold off
-
-% function addTrap(x,y,n,type)
-%     fplot(,type)
-% end
+end
--- newscript.m
+++ newscript.m
@@ -1,29 +1,27 @@
+function [ret] = newscript(x, y, xerr)
 %Created BY Ofir Arad, Feb-6-2019.
 %This script fits a non-linear model described by variable "test_model" to
 %the data given by {xdata,ydata} with errors {edata}.
- 
-test_model='y~(b1*x+b2)';%model to be fitted, coefficeients are of the form b#
+cosiderXerr = true;
+test_model='y~(b1*x+b2)';%model t   o be fitted, coefficeients are of the form b#
 beta0=[1 1];%starting values for the coefficients
  
 xdata=x; %x-data points
 ydata=y; %y-data points
-edata=ones(1,length(ydata))*0.1; % errors in y-data points
+
+edata=ones(1,length(ydata))/16000; % errors in y-data points
+
 xErr = xerr;
 dd=1./(edata.*edata);
 %creating the model, errors for this model based on sigma^{-2}
+% opts=statset('TolX',1e-300,'TolFun',1e-300);
 temp_nlm=fitnlm(xdata,ydata,test_model,beta0,'Weights',dd);
-%temp_nlm=fitnlm(xdata,ydata,test_model,beta0,'Weights',edata.^(-2));
-
-slope = temp_nlm.Coefficients(1,1);
-i=1;
-for xe = xErr.'
-   edata(i) = edata(i) + slope.Estimate*2*xe;
-   i = i + 1;
+if cosiderXerr
+    slope = temp_nlm.Coefficients(1,1);
+    edata = edata + xErr.'.*slope.Estimate*2;
+    dd=1./(edata.*edata);
+    temp_nlm=fitnlm(xdata,ydata,test_model,beta0,'Weights',dd);
 end
-
-dd=1./(edata.*edata);
-%creating the model, errors for this model based on sigma^{-2}
-temp_nlm=fitnlm(xdata,ydata,test_model,beta0,'Weights',dd);
 
 temp_coeff=cell2mat(table2cell(temp_nlm.Coefficients));%fit coefficients
  
@@ -43,15 +41,13 @@ errorbar(xdata,ydata,edata,'b*');
 hold on
 xdiff=(max(xdata)-min(xdata));%just the difference between the x values
 fplot(@(x) (temp_coeff(1,1)*x+temp_coeff(2,1)),[min(xdata)-xdiff/10,max(xdata)+xdiff/10]);
-title('Voltage vs Radius Squared','Interpreter','latex');
-ylabel('Voltage [V]');
-xlabel('Radius squared [m^2]');    
+title('Big Ring Radius vs Wavelength','Interpreter','latex');
+ylabel('Radius [m]');
+xlabel('\lambda [nm]');
 % addTrap(x,y,3,'-');
 % addTrap(x,y,6,'r');
 % addTrap(x,y,18,'y')
 text(min(xdata),max(ydata),sprintf('y=b_1x+b_2\nb1=%d\\pm%d\nb2=%d\\pm%d\n\\chi^2=%d\nd.o.f-reduced \\chi^2=%d',temp_coeff(1,1),temp_coeff(1,2),temp_coeff(2,1),temp_coeff(2,2),temp_nlm.SSE,temp_nlm.MSE),'verticalAlignment','top')
 hold off
 
-% function addTrap(x,y,n,type)
-%     fplot(,type)
-% end
+end
